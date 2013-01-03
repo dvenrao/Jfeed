9574056994b432df75a9824086b330d5b78d1ef8
venky-D <venkat@startupsourcing.com>
2013-01-03 16:16:14 +0530

    all links_to search

--- app/models/job.rb
+++ app/models/job.rb
@@ -21,10 +21,59 @@ class Job < ActiveRecord::Base
     ]
   end
 
+  def self.category
+    [
+      ["Select a Category"],
+      ["Accounting / Auditing / Tax"],
+      ["Administration / Secretary / Front Office"],
+      ["Architecture / Civil Engineering"],
+      ["Art / Design / Creative / Fashion"],
+      ["Banking / Financial Services"],
+      ["Construction"],
+      ["Consulting / Strategy / Corporate Planning"],
+      ["Content / Edition / Journalism"],
+      ["Customer Service / Call Centre / Operations / Data Entry"],
+      ["Education / Teaching / Language"],
+      ["Engineering / R&amp;D"],
+      ["Executive Management"],
+      ["Freshers Jobs"],
+      ["Healthcare / Medical / Pharmacy"],
+      ["Hotel / Restaurant / Catering"],
+      ["HR / Recruitment"],
+      ["Import-Export / Merchandising / Trading"],
+      ["Insurance"],
+      ["Internet Technologies / Web / E-Commerce"],
+      ["IT - Databases / Datawarehousing"],
+      ["IT - ERP / CRM"],
+      ["IT - Hardware / Telecom / Support"],
+      ["IT - Project Management"],
+      ["IT - Software Programming / Analysis"],
+      ["IT - Systems / Networking / Security"],
+      ["Legal / Law"],
+      ["Logistics / Purchase / Supply Chain / Procurement"],
+      ["Manufacturing / Packaging / Printing / Industrial Jobs"],
+      ["Marketing / Communication / Advertising / PR"],
+      ["Mechanical Engineering / Mechatronics"],
+      ["Media / TV / Films / Production"],
+      ["Pharma, Biotech and Chemical Industry"],
+      ["Project Management"],
+      ["Quality / Testing / Process Control"],
+      ["Real Estate / Property"],
+      ["Sales / Business Development"],
+      ["Security Services / Guards"],
+      ["Skilled Trade / Service / Installation / Repair"],
+      ["Social Services / NGOs / Nonprofit"],
+      ["Travel / Reservation / Airlines"]
+    ]
+  end
+
 
   def self.search(search)
     if search
       find(:all, :conditions => ['position LIKE ?', "%#{search}%"])
+      if search == "Fresher"
+        find(:all, :conditions => ['experience LIKE ?', "%#{search}%"])
+      end
     else
       find(:all)
     end
